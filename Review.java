9b0cd1ed3130203f0f1db3809166e565af67ebd0
Tanisha Hossain <101995310+tanisha-hossain@users.noreply.github.com>
2025-01-16 04:17:51 -0500

    Create Review.java

---
+++ Review.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Review {
+    private String username;
+    private String text;
+
+    public Review(String username, String text) {
+        this.username = username;
+        this.text = text;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String toString() {
+        return username + " says '" + text + "'\n";
+    }
+}
+
+
