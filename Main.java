f3846b6c97697e40f0532ecb8ae1b35b5636fd20
Tanisha Hossain <101995310+tanisha-hossain@users.noreply.github.com>
2025-01-16 15:14:16 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,15 +14,56 @@ public class Main {
     System.out.print("Enter your longitude: ");
     double longitude = userInput.nextDouble();
     userInput.nextLine(); 
+  
     
     User user = new User(username, latitude, longitude); 
     ParkManager manager = new ParkManager();
     
-    manager.displayAllParks("src/main/java/betterdataset.csv"); //reminder: on replit we have to copy file path
+    manager.readParksFile("src/main/java/betterdataset.csv"); //reminder: on replit we have to copy file path
+    manager.displayAllParksByDistance(user);
     manager.ratePark(userInput);
     manager.leaveReview(userInput, user);
-    manager.displayAllParks("src/main/java/betterdataset.csv");
+    manager.displayAllParks();
 
+
+
+    /*public double[] userLocation(Scanner userInput) {
+      double[] userCoordinates = new double[2];
+      boolean valid = false;
+
+    while (!valid) {
+      try {
+          System.out.println("Enter your latitude (-90 to 90):");
+          double latitude = userInput.nextDouble();
+
+          if (latitude < -90 || latitude > 90) {
+              System.out.println("Invalid latitude. Please enter a value between -90 and 90.");
+              continue;
+          }
+        
+          System.out.println("Enter your longitude (-180 to 180):");
+          double longitude = userInput.nextDouble();
+
+          if (longitude < -180 || longitude > 180)
+            System.out.println("Invalid longitude. Please enter a value between -180 and 180.");
+            continue;
+    
+      
+          userCoordinates[0] = latitude;
+          userCoordinates[1] = longitude;
+          valid = true;
+      
+    } catch(InputMismatchException e) {
+      System.out.println("Invaild input. Please enter a numeric value for both latitude and longitude.");
+    }
+        }
+      System.out.printf("Your location is set to Latitude: %.2f, Longitude: %.2f\n", userCoordinates[0], userCoordinates[1]);
+      return userCoordinates;
+
+}*/
+
+        
+        
   } //end main
 
 } //end class
