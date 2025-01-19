ef884cae8246d35758904b86de03ea0877593307
nesteabestie <faizakibria1@gmail.com>
2025-01-18 23:13:17 -0500

    Update Parkometer.java

--- Parkometer.java
+++ Parkometer.java
@@ -11,15 +11,16 @@ import java.io.*;
 
 public class Parkometer {
     public static void main(String[] args) {
-        System.out.println("Welcome to Park-O-Meter!");
+        
+        System.out.println(color.yellowText + "Welcome to Park-O-Meter!");
         Scanner userInput = new Scanner(System.in);
 
         //get user information
-        System.out.print("Enter your username: ");
+        System.out.print(color.blueText + "Enter your username: ");
         String username = userInput.nextLine();
-        System.out.print("Enter your latitude: ");
+        System.out.print(color.greenText + "Enter your latitude: ");
         double latitude = userInput.nextDouble();
-        System.out.print("Enter your longitude: ");
+        System.out.print(color.greenText + "Enter your longitude: ");
         double longitude = userInput.nextDouble();
         userInput.nextLine(); 
         User user = new User(username, latitude, longitude);
@@ -30,29 +31,27 @@ public class Parkometer {
 
         navigationMenu(user, manager, userInput); //user navigates through the program with the menu
 
-        System.out.println("Thank you for using Park-O-Meter! Enjoy your day out :)");  //after user chooses to exit
+        System.out.print(color.boldText + color.yellowText + "Thank you for using Park-O-Meter! Enjoy your day out :)" + color.unbold);  //after user chooses to exit
     } //end main
     
     
     // Gives user choices of what to do in the program
     public static void navigationMenu(User user, ParkManager manager, Scanner userInput) {
-        String boldText = "\033[0;1m";
-        String unbold = "\033[0;0m";
         boolean exit = false;
 
         while(!exit) {
-            System.out.println(boldText + "\nPark-O-Meter Main Menu" + unbold);
-            System.out.println("What can Park-O-Meter do for you today? Select an option by entering the corresponding number from the list below:");
-            System.out.println("\n1 - Display All Parks");
-            System.out.println("2 - Display Parks by Distance");
-            System.out.println("3 - Display Parks by Ratings");
-            System.out.println("4 - Rate a Park");
-            System.out.println("5 - Leave a Review");
-            System.out.println("6 - Filter Parks");
-            System.out.println("7 - Create a Park Loop");
-            System.out.println("8 - Change your current Location");
-            System.out.println("9 - Exit");
-            System.out.print("Enter your choice: ");
+            System.out.println(color.boldText + "\nPark-O-Meter Main Menu" + color.unbold);
+            System.out.println(color.cyanText + "What can Park-O-Meter do for you today? Select an option by entering the corresponding number from the list below:");
+            System.out.println(color.whiteText + "\n1 - Display All Parks");
+            System.out.println(color.whiteText + "2 - Display Parks by Distance");
+            System.out.println(color.whiteText + "3 - Display Parks by Ratings");
+            System.out.println(color.whiteText + "4 - Rate a Park");
+            System.out.println(color.whiteText + "5 - Leave a Review");
+            System.out.println(color.whiteText + "6 - Filter Parks");
+            System.out.println(color.whiteText + "7 - Create a Park Loop");
+            System.out.println(color.whiteText + "8 - Change your current Location");
+            System.out.println(color.whiteText + "9 - Exit");
+            System.out.print(color.cyanText + color.blackBG + "Enter your choice: " + color.RESET);
 
             int choice; //user chooses what they want to do through an integer number representing actions as listed in the strings above
             try {
@@ -88,11 +87,11 @@ public class Parkometer {
                         exit = true;
                         break;
                     default:
-                        System.out.println("Invalid choice. Please try again.");
+                        System.out.println(color.redText + "Invalid choice. Please try again.");
                         break;
                 }
             } catch (InputMismatchException e) {
-                System.out.println("Invalid choice. Please enter a number between 1 and 7.");
+                System.out.println(color.redText + "Invalid choice. Please enter a number between 1 and 7.");
                 userInput.nextLine(); 
                 continue;
             }
